7eff98a14a73a299f4b5e5d8f31aa82fef9c856c
Jacob Swartwood <jswartwood@gmail.com>
2011-02-04 14:04:45 -0500

    Fixing some bugs: toggleClass(undefined) and removeClass(undefined) work as expected, events fire for each class changed on each element. Also added optional namespacing to reduce event handler runs and class testing logic.

--- jQuery-classEvents.js
+++ jQuery-classEvents.js
@@ -4,21 +4,59 @@
  * Copyright (c) 2011 Jacob Swartwood
  */
 
-(function( $ ) {
-	// TODO: prevent applying this plugin more than once...
-	var evts = [ "addClass", "removeClass" ];
+(function( $, undefined ) {
+	if ($.classchangeNamespace !== undefined) return;
+	
+	$.classchangeNamespace = false;
+	
+	function evtName( val ) {
+		return "classchange" + ($.classchangeNamespace ? "." + val : "");
+	}
+	
+	var CHANGE_EVT = "classchange"
+		evts = [
+			{
+				"name": "addClass",
+				"func": function( value ) {
+					value = (value || "").split(" ");
+					for (var i = 0, j = value.length; i < j; i++) {
+						$(this).triggerHandler(evtName(value[i]), { "class": value[i], "action": "add" });
+					}
+				}
+			},
+			{
+				"name": "removeClass",
+				"func": function( value, oldClassName ) {
+					value = (value || oldClassName || "").split(" ");
+					for (var i = 0, j = value.length; i < j; i++) {
+						$(this).triggerHandler(evtName(value[i]), { "class": value[i], "action": "remove" });
+					}
+				}
+			},
+			{
+				"name": "toggleClass",
+				"func": function( value, oldClassName ) {
+					var newClassName = this.className;
+					if ($.type(value) !== "string" && oldClassName !== newClassName) {
+						value = (newClassName || oldClassName || "").split(" ");
+						for (var i = 0, j = value.length; i < j; i++) {
+							$(this).triggerHandler(evtName(value[i]), { "class": value[i], "action": oldClassName ? "add" : "remove" });
+						}
+					}
+				}
+			}
+		];
 	
 	for (var i = 0, j = evts.length; i < j; i++) {
 		(function( action ) {
-			var oldAction = $.fn[action];
-			$.fn[action] = function( value ) {
-				var that = oldAction.apply(this, Array.prototype.slice.call(arguments));
-
-				if ($.type(value) === "string") {
-					this.trigger("classchange", { "type": action, "class": value });
-				}
-
-				return that;
+			var oldAction = $.fn[action.name];
+			$.fn[action.name] = function( value ) {
+				var args = Array.prototype.slice.call(arguments);
+				return this.each(function() {
+					var oldClassName = this.className;
+					oldAction.apply($(this), args);
+					action.func.call(this, value, oldClassName);
+				});
 			};
 		})(evts[i]);
 	}
